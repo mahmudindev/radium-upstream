7d18cc99c111eb7bd5aed2eea44a653f33e2e455
M. Mahmudin <42290713+mahmudindev@users.noreply.github.com>
2025-05-29 20:33:51 +0700

    Port mixin mod overrides

--- src/main/java/me/jellysquid/mods/lithium/common/config/LithiumConfig.java
+++ src/main/java/me/jellysquid/mods/lithium/common/config/LithiumConfig.java
@@ -7,16 +7,54 @@ import net.caffeinemc.caffeineconfig.CaffeineConfig;
 import net.caffeinemc.caffeineconfig.Option;
 import net.minecraftforge.fml.loading.FMLPaths;
 import net.minecraftforge.fml.loading.LoadingModList;
+import net.minecraftforge.fml.loading.moddiscovery.ModInfo;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Map;
 import java.util.Properties;
 
 public class LithiumConfig extends AbstractCaffeineConfigMixinPlugin {
 
     private CaffeineConfig applyLithiumCompat(CaffeineConfig config) {
+        // https://github.com/CaffeineMC/lithium/blame/develop/neoforge/src/main/java/net/caffeinemc/mods/lithium/neoforge/NeoForgeMixinOverrides.java#L20
+        // https://github.com/CaffeineMC/lithium/blame/develop/common/src/main/java/net/caffeinemc/mods/lithium/common/config/LithiumConfig.java#L168
+        for (ModInfo meta : LoadingModList.get().getMods()) {
+            meta.getOwningFile().getConfigElement("lithium:options").ifPresent(override -> {
+                if (override instanceof Map<?, ?> overrides && overrides.keySet().stream().allMatch(key -> key instanceof String)) {
+                    overrides.forEach((key, value) -> {
+                        if (!(value instanceof Boolean) || !(key instanceof String)) {
+                            System.out.printf("[Radium] Mod '%s' attempted to override option '%s' with an invalid value, ignoring", meta.getModId(), key);
+                            return;
+                        }
+
+                        Option option = config.getOption((String) key);
+                        if (option == null && !((String) key).startsWith("mixin.")) {
+                            option = config.getOption("mixin." + key);
+                        }
+
+                        if (option == null) {
+                            System.out.printf("[Radium] Mod '%s' attempted to override option '%s', which doesn't exist, ignoring", meta.getModId(), key);
+                            return;
+                        }
+
+                        // disabling the option takes precedence over enabling
+                        if (!((Boolean) value) && option.isEnabled()) {
+                            option.clearModsDefiningValue();
+                        }
+
+                        if (!((Boolean) value) || option.isEnabled() || option.getDefiningMods().isEmpty()) {
+                            option.addModOverride((Boolean) value, meta.getModId());
+                        }
+                    });
+                } else {
+                    System.out.printf("[Radium] '%s' contains invalid Lithium option overrides, ignoring", meta.getModId());
+                }
+            });
+        }
+
         if (LoadingModList.get().getModFileById("ferritecore") != null) { // https://github.com/malte0811/FerriteCore/blob/1.20.0/Fabric/src/main/resources/fabric.mod.json#L38
             config.getOption("mixin.alloc.blockstate").addModOverride(false, "ferritecore");
         }
